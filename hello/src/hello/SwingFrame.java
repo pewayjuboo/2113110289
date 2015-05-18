2aecf49bf99a013b6463606ec1134dbef5e0ac27
kieu <kieu@huykieu>
2015-05-18 19:33:11 +0700

    hi

--- hello/src/hello/SwingFrame.java
+++ hello/src/hello/SwingFrame.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 public class SwingFrame extends JFrame{
 	  public static void main( String args[] ) {
 			JFrame win = new JFrame( "My First GUI Program" );
-			JLabel label = new JLabel( "Hello World" );
+			JLabel label = new JLabel( "Hello Bao Ngan" );
 			win.setBounds(30, 30, 300, 300); 
 			win.getContentPane().add( label );
 
