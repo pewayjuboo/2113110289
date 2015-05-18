b1f757adae660950836641c20fc5f8ce52efbe15
pewayjuboo <pewayrute1995@gmail.com>
2015-05-18 16:10:28 +0700

    hello 1

---
+++ hello/src/hello/SwingFrame.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SwingFrame extends JFrame{
+	  public static void main( String args[] ) {
+			JFrame win = new JFrame( "My First GUI Program" );
+			JLabel label = new JLabel( "Hello World" );
+			win.setBounds(30, 30, 300, 300); 
+			win.getContentPane().add( label );
+
+			win.setVisible(true);
+		    }
+}
